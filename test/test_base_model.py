0fdbc8d10bbd2293fba1b8c7b5ae13d22acb3ee5
Breign3 <brainy.ngwenya@gmail.com>
2024-06-10 06:36:50 -0700

    Base_model.py

--- test/test_base_model.py
+++ test/test_base_model.py
@@ -1,54 +1,13 @@
-# tests/test_base_model.py
-
-import unittest
 from models.base_model import BaseModel
-from datetime import datetime
-import uuid
 
-class TestBaseModel(unittest.TestCase):
-
-    def setUp(self):
-        """Set up for the tests"""
-        self.model = BaseModel()
-
-    def test_id(self):
-        """Test if id is a string"""
-        self.assertIsInstance(self.model.id, str)
-
-    def test_uuid(self):
-        """Test if id is a valid uuid"""
-        try:
-            uuid.UUID(self.model.id, version=4)
-        except ValueError:
-            self.fail("id is not a valid uuid")
-
-    def test_created_at(self):
-        """Test if created_at is a datetime object"""
-        self.assertIsInstance(self.model.created_at, datetime)
-
-    def test_updated_at(self):
-        """Test if updated_at is a datetime object"""
-        self.assertIsInstance(self.model.updated_at, datetime)
-
-    def test_save(self):
-        """Test if save method updates updated_at"""
-        old_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(old_updated_at, self.model.updated_at)
-
-    def test_to_dict(self):
-        """Test to_dict method"""
-        model_dict = self.model.to_dict()
-        self.assertEqual(model_dict['id'], self.model.id)
-        self.assertEqual(model_dict['created_at'], self.model.created_at.isoformat())
-        self.assertEqual(model_dict['updated_at'], self.model.updated_at.isoformat())
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-
-    def test_str(self):
-        """Test __str__ method"""
-        expected = "[BaseModel] ({}) {}".format(self.model.id, self.model.__dict__)
-        self.assertEqual(str(self.model), expected)
-
-if __name__ == '__main__':
-    unittest.main()
-
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
